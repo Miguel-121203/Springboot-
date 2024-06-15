c7955ad29c83691871c71042da994e373154ecb2
Miguel Angel <miguelangel.br1203@gmail.com>
2024-06-14 21:38:17 -0500

    creacion de modelo User

--- springboot-web/src/main/java/com/miguel/webapp/springboot_web/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/miguel/webapp/springboot_web/controllers/UserRestController.java
@@ -1,21 +1,25 @@
 package com.miguel.webapp.springboot_web.controllers;
 
+import com.miguel.webapp.springboot_web.models.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
 
-  @GetMapping("/details2")
-  public Map<String, Object>  details(){
+  @GetMapping("/details")
+  public Map<String, Object> details() {
+
+    User user = new User("Miguel Angel", "Beltran Bohorquez");
     Map<String, Object> body = new HashMap<>();
 
-    body.put("title","Hola Mundo Spring Boot");
-    body.put("name","Miguel Angel");
-    body.put("lastname","Beltran Bohorquez");
+    body.put("title", "Hola Mundo Spring Boot");
+    body.put("user", user);
     return body;
   }
 
